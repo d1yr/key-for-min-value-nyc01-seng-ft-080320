f6d01907a82f1b4a22cb230848fed2716c5b32e8
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 04:09:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 lowest_value = 0
   lowest_key = nil
-  
+  #have to set lowest_key to nil to return nil value
   name_hash.each do |k,v|
     if lowest_value == 0 || v < lowest_value
       lowest_value = v
