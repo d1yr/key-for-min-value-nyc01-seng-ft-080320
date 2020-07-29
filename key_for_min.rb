ff2336d33c50ad56c7cc7950606abcf0dcdbc1c0
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 04:10:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 lowest_value = 0
-  lowest_key = nil
+  lowest_key = false
   #have to set lowest_key to nil to return nil value
   name_hash.each do |k,v|
     #tells method to compare lowest value to 0 and print key, which was set to nil
