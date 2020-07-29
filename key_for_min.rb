7269b917b3f8a0a083afeefe1e89e8a408f8f636
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 04:02:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.sort!{|k, v| return v.first }
+name_hash.!{|k, v| return v.first }
 end
